2980de3554e94862713cfbb50c0dee0015ee766d
CrespinR <bryanpppe@gmail.com>
2026-09-11 18:54:33 -0600

    icono de app y cambio de colores

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.example.desafio2dsm"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
